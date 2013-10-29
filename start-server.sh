bbdb5f2c36552a4fc0b60504c49d0710d0856325
yangqi3 <sloanyang@gmail.com>
2013-10-29 10:54:24 +0800

    Visualizer booted up

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 export MYIP="$(/sbin/ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')"
-export PATH=$PATH:/home/yangq/google_appengine/
-dev_appserver.py --host=$MYIP --port=8180 --admin_host=$MYIP --admin_port=8100 . 
+export PATH=$PATH:/home/yangqi3/bin/google_appengine/
+dev_appserver.py --address=10.0.2.15 --port=8080 . 
 exit
