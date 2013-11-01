86ff89762895a92e08ee633a1a19a490f38c769a
yangqi3 <sloanyang@gmail.com>
2013-11-01 14:48:24 +0800

    Production Env Done

--- webkit-perf/start-server.sh
+++ webkit-perf/start-server.sh
@@ -1,4 +1,4 @@
 export MYIP="$(/sbin/ifconfig eth1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')"
 export PATH=$PATH:/home/yangqi3/bin/google_appengine/
-dev_appserver.py --host=$MYIP --port=8080 --admin_host=$MYIP --admin_port=8090 .
+dev_appserver.py --host=$MYIP --port=8080 --admin_host=$MYIP --admin_port=8090 --storage_path=$PWD/appengine.webkit-perf.yangqi3 .
 exit
